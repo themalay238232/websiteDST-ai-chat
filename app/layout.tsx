73fc72c20406798bee3c06cbd99dbbf3bc6fc8d0
Theluc <quyenoppo205@gmail.com>
2026-07-18 15:20:32 +0700

    Fix Vietnamese heading font rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,500;9..144,700;9..144,800&family=Source+Sans+3:wght@400;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@600;700;800&family=Source+Sans+3:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
